97dd5308aecde5fa5d667cebae1026aa33bd535b
GabrielC <gabriel.camargo96@hotmail.com.br>
2022-07-06 17:52:43 -0300

    chore: add router to main app file
    this new router should be used for now on to create new routes.
    it is being used in app.js

--- src/app.js
+++ src/app.js
@@ -1,18 +1,14 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import router from './routers/routes';
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 app.use(cors());
-
-app.get('/', (req, res) => {
-    res.send('ta online')
-})
-
-// teste
+app.use(router);
 
 const PORT = process.env.PORT;
 app.listen(PORT);
