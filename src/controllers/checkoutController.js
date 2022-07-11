bd34848e23764cec3f052d064c1e60489dd3ffd8
GabrielC <gabriel.camargo96@hotmail.com.br>
2022-07-11 18:43:13 -0300

    refactor: change file style

--- src/controllers/checkoutController.js
+++ src/controllers/checkoutController.js
@@ -1,14 +1,15 @@
-import { cart } from "../models/index.js";
+import { cart } from '../models/index.js';
 
 export const deleteCart = async (req, res) => {
-    const {cartId} = res.params
-    try {
-        const userCart = await cart.deleteCartById(cartId)
-        if (!userCart) {
-          return res.status(422).send('não foi possível cancelar este carrinho')
-        } 
-        res.status(201).send('OK')
-    } catch (error) {
-        res.status(500).send(error)
+  const { cartId } = res.params;
+  try {
+    const userCart = await cart.deleteCartById(cartId);
+    if (!userCart) {
+      return res.status(422).send('não foi possível cancelar este carrinho');
     }
-}
+    return res.status(201).send('OK');
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send(error);
+  }
+};
